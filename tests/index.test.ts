557a70955c765145abd147c3cd578c74f93c6f49
Awal Ariansyah <awalariansyah7@gmail.com>
2025-11-20 15:20:19 +0700

    init test

---
+++ tests/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, test } from "bun:test";
+
+describe("FaceService Initialization", () => {
+  test("should initialize with default models from GitHub", async () => {
+    // Lorem
+  }, 30000); // Increase timeout for download
+});
